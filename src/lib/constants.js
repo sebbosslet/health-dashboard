89a4b9e1762f5cbaabb5e8e8d763a0229161a80f
Sebs Health <build@sebs.health>
2026-04-17 13:48:20 +0000

    Fix: malformed ALCOHOL_REGEX was crashing the app on load

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -10,4 +10,4 @@ export const CAFFEINE_HALF_LIFE_HOURS = 5
 export const IMG_MAX_PX = 800
 export const IMG_QUALITY = 0.75
 
-export const ALCOHOL_REGEX = /\bbeer\b|\bbeer\b|\blager\b|\bale\b|\bipa\b|\bstout\b|\bwine\b|\brosé\b|\brose wine\b|\bchampagne\b|\bprosecco\b|\bsparkling wine\b|\bwhisky\b|\bwhiskey\b|\bbourbon\b|\bscotch\b|\bvodka\b|\bgin\b|\brum\b|\btequila\b|\bmezcal\b|\bcognac\b|\bbrandy\b|\bcocktail\b|\bmargarita\b|\bmojito\b|\bnegroni\b|\bspritz\b|\baperол\b|\bapertif\b|\bdigestif\b|\bliqueur\b|\bshots?\b|\bpint\b|\bhalf pint\b|\bdrink\b.*alcohol|\balcohol/i
+export const ALCOHOL_REGEX = /\bbeer\b|\blager\b|\bale\b|\bipa\b|\bstout\b|\bwine\b|\bchampagne\b|\bprosecco\b|\bwhisky\b|\bwhiskey\b|\bbourbon\b|\bscotch\b|\bvodka\b|\bgin\b|\brum\b|\btequila\b|\bmezcal\b|\bcognac\b|\bbrandy\b|\bcocktail\b|\bmargarita\b|\bmojito\b|\bnegroni\b|\bspritz\b|\bapertif\b|\bdigestif\b|\bliqueur\b|\bshots?\b|\bpint\b|\balcohol\b/i
